db0f5195edf57fd892d2350277ec49f8c263f366
ramonvaleriano <rrvaleriano@gmail.com>
2022-07-08 18:04:18 -0300

    [0.0.1]-[20220708]_4-comecando-a-desenvolver-as-tabelas-pelas-models

---
+++ AulasDoCurso/Aula001/models/tipo_embalagem.py
@@ -0,0 +1,14 @@
+import sqlalchemy
+from datetime import datetime
+from models.model_base import ModelBase
+
+
+class TipoEmbalagem(ModelBase):
+    __tablename__ = 'tipos_embalagens'
+
+    id: int = sqlalchemy.Column(sqlalchemy.BigInteger, primary_key=True, autoincrement=True)
+    data_criacao: datetime = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.now(), index=True)
+    nome: str = sqlalchemy.Column(sqlalchemy.String(45), unique=True, nullable=False)
+
+    def __repr__(self) -> str:
+        return f'<Tipo de Embalagem: {self.nome}>'
---
+++ AulasDoCurso/Aula001/models/tipo_picole.py
@@ -0,0 +1,15 @@
+import sqlalchemy
+from datetime import datetime
+from models.model_base import ModelBase
+
+
+class TipoPicole(ModelBase):
+
+    __tablename__ = 'tipos_picole'
+
+    id: int = sqlalchemy.Column(sqlalchemy.BigInteger, primary_key=True, autoincrement=True)
+    data_criacao: datetime = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.now(), index=True)
+    nome: str = sqlalchemy.Column(sqlalchemy.String(45), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<Tipo de Picole: {self.nome}>'
